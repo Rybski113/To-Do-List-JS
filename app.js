372f7ee927815417a4228e6169fa062ba4648dcd
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-08-10 16:53:18 +0200

    alert if input is empty

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ let toDoContainer = document.getElementById('toDoContainer');
 
 
 toDoButton.addEventListener('click', function() {
+    
     let paragraph = document.createElement('p');
     paragraph.classList.add('paragraph-styling');
     paragraph.innerText = inputField.value;  
     toDoContainer.appendChild(paragraph); 
+    if( inputField.value == "") {
+        alert("Please Enter a Task")
+    }
     inputField.value = ""; 
     paragraph.addEventListener('click', function() {
         paragraph.style.background = "red";
