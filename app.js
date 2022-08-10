53b876cdfcc9a5c4033602255dfae6a35c60e9c7
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-08-10 17:35:15 +0200

    button in paragraph added

--- app.js
+++ app.js
@@ -4,21 +4,27 @@ let toDoContainer = document.getElementById('toDoContainer');
 
 
 toDoButton.addEventListener('click', function() {
-    
-    let paragraph = document.createElement('p');
-    paragraph.classList.add('paragraph-styling');
-    paragraph.innerText = inputField.value;  
-    toDoContainer.appendChild(paragraph); 
     if( inputField.value == "") {
         alert("Please Enter a Task")
+    } else {
+        let paragraph = document.createElement('p');
+        paragraph.classList.add('paragraph-styling');
+        paragraph.innerText = inputField.value;  
+        let closeButton = document.createElement("button");
+        paragraph.appendChild(closeButton);
+        
+        
+        toDoContainer.appendChild(paragraph); 
+        
+        inputField.value = ""; 
+        paragraph.addEventListener('click', function() {
+            paragraph.style.background = " rgb(199, 117, 117)";
+        })
+        paragraph.addEventListener('dblclick', function() {
+            toDoContainer.removeChild(paragraph);
+        })
     }
-    inputField.value = ""; 
-    paragraph.addEventListener('click', function() {
-        paragraph.style.background = "red";
-    })
-    paragraph.addEventListener('dblclick', function() {
-        toDoContainer.removeChild(paragraph);
-    })
+    
 })
 
 
