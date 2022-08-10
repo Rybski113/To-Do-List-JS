aa4b92532afede8271a5964ef118fcfde4c6158e
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-08-10 16:50:31 +0200

    Simple todolist

---
+++ app.js
@@ -0,0 +1,20 @@
+let toDoButton = document.getElementById('addToDo');
+let inputField = document.getElementById('inputField');
+let toDoContainer = document.getElementById('toDoContainer');
+
+
+toDoButton.addEventListener('click', function() {
+    let paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value;  
+    toDoContainer.appendChild(paragraph); 
+    inputField.value = ""; 
+    paragraph.addEventListener('click', function() {
+        paragraph.style.background = "red";
+    })
+    paragraph.addEventListener('dblclick', function() {
+        toDoContainer.removeChild(paragraph);
+    })
+})
+
+
