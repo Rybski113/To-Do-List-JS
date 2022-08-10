6d937be1cbd325ba0de5779991fac5fb86542789
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-08-10 17:47:40 +0200

    number of tasks displayed

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 let toDoButton = document.getElementById('addToDo');
 let inputField = document.getElementById('inputField');
 let toDoContainer = document.getElementById('toDoContainer');
+let resultDisplay = document.getElementById('result');
+
+let result = 1
+
 
 
 toDoButton.addEventListener('click', function() {
@@ -12,11 +16,11 @@ toDoButton.addEventListener('click', function() {
         paragraph.innerText = inputField.value;  
         let closeButton = document.createElement("button");
         paragraph.appendChild(closeButton);
-        
-        
         toDoContainer.appendChild(paragraph); 
-        
+        resultDisplay.innerHTML = result
+        result ++
         inputField.value = ""; 
+
         paragraph.addEventListener('click', function() {
             paragraph.style.background = " rgb(199, 117, 117)";
         })
